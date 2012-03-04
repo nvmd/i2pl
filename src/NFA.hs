1ba42de8486982e4d6661919ebc621d03d55e31c
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-03-05 01:33:58 +0400

    NFA operation
    Check if the word is accepted or rejected by the automation. Additional changes may be required to support epsilon-transitions.

--- src/NFA.hs
+++ src/NFA.hs
@@ -2,9 +2,13 @@
 module NFA
 (   NFA (NFA)
 ,   Delta (Delta, EDelta)
+,   nfa_next_states
+,   nfa_delta
+,   nfa_final_states
+,   nfa_accepts_word
 ) where
 
-import Data.Set (Set, toList, difference)
+import Data.Set (Set, toList, difference, fromList, member)
 
 data NFA q = NFA             -- Non-deterministic Finite state Automation (Char - set of input symbols)
              (Set q)         -- set of states (Q)
@@ -25,10 +29,10 @@ instance (Show q, Ord q) => Show (NFA q) where
 --                              ++ ")"
     show (NFA q delta q0 f) = "digraph Nfa {\n"
                               ++ "\t\"\" [shape=none];\n"
-                              ++ foldl (\s q -> s ++ "\t\"" ++ q ++ "\" [shape=circle];\n")
+                              ++ foldl (\s qi -> s ++ "\t\"" ++ qi ++ "\" [shape=circle];\n")
                                        ""
                                        (map (show) (toList (difference q f)))
-                              ++ foldl (\s q -> s ++ "\t\"" ++ q ++ "\" [shape=doublecircle];\n")
+                              ++ foldl (\s qi -> s ++ "\t\"" ++ qi ++ "\" [shape=doublecircle];\n")
                                        ""
                                        (map (show) (toList f))
                               ++ "\n\t\"\" -> " ++ show q0 ++ ";\n"
@@ -40,3 +44,31 @@ instance Show q => Show (Delta q) where
 --    show (EDelta q1 q2)  = "(" ++ show q1 ++ ",e," ++ show q2 ++ ")"
     show (Delta q1 c q2) = "\t" ++ show q1 ++ " -> " ++ show q2 ++ " [label=" ++ show (c:[]) ++ "];\n"
     show (EDelta q1 q2)  = "\t" ++ show q1 ++ " -> " ++ show q2 ++ " [label=\\epsilon];\n"
+
+nfa_delta :: (Ord q) => (NFA q) -> q -> Char -> (Set q)
+nfa_delta (NFA _ delta _ _) state symbol = fromList [ q2 | (Delta q1 c q2) <- (toList delta), q1 == state, c == symbol]
+
+nfa_next_states :: (Ord q) => (NFA q) -> (Set q) -> Char -> (Set q)
+nfa_next_states nfa states symbol = fromList (foldl (\state_list qi -> (toList (nfa_delta nfa qi symbol)) ++ state_list)
+                                              []
+                                              (toList states))
+
+-- Returns set of states that NFA will be in after reading the whole word
+nfa_final_states :: (Ord q) => (NFA q) -> String -> (Set q)
+nfa_final_states (NFA q delta q0 f) word = nfa_final_states_from_state (NFA q delta q0 f) (fromList [q0]) word
+
+-- Returns set of states that NFA will be in after reading the whole word, assuming the supplied set of start states
+nfa_final_states_from_state :: (Ord q) => (NFA q) -> (Set q) -> String -> (Set q)
+nfa_final_states_from_state _ states []       = states
+nfa_final_states_from_state nfa states (w:ws) = nfa_final_states_from_state nfa (nfa_next_states nfa states w) ws
+
+-- Check, whether NFA accepts the word, or not
+nfa_accepts_word :: (Ord q) => (NFA q) -> String -> Bool
+nfa_accepts_word (NFA q delta q0 f) word = list_has_elem True
+                                           (map (\q -> member q f) (toList (nfa_final_states (NFA q delta q0 f) word)))
+
+list_has_elem :: (Eq q) => q -> [q] -> Bool
+list_has_elem _ [] = False
+list_has_elem elem (head:tail)
+    | head == elem = True
+    | otherwise    = list_has_elem elem tail
